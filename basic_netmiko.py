1ffdb1ae63445dd4e942a6a798f54de16cadc87b
Ashish Bhosale <aashishbhosale10@gmail.com>
2021-02-26 12:41:19 -0600

    Update basic_netmiko.py

--- basic_netmiko.py
+++ basic_netmiko.py
@@ -2,6 +2,6 @@
 From netmiko import Netmiko 
 
 Connection =Netmiko(host='191.168.101.148', port='22', username='cisco', password='cisco',device_type='cisco_ios')
-Output = connection.end_command('sh ip int br')
+Output = connection.send_command('sh ip int br')
 Print(output) 
 Print('Closingconnection')Connection.disconnect()
